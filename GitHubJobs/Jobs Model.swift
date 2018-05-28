6e72b22c1214bfaef1fb66d2d670883c973369c3
Unknown <benadams@me.com>
2018-05-28 15:21:16 -0600

    This is the beginning

---
+++ GitHubJobs/Jobs Model.swift
@@ -0,0 +1,13 @@
+//
+//  Jobs Model.swift
+//  GitHubJobs
+//
+//  Created by Ben Adams on 5/28/18.
+//  Copyright © 2018 Ben Adams. All rights reserved.
+//
+
+import Foundation
+
+struct Jobs: Codable {
+    
+}
